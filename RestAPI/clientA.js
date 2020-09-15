8ad5a8258a49cdeb918eeaac31307ccf3e43b75d
Setthanan <42529434+Setthanan@users.noreply.github.com>
2020-09-15 22:49:11 +0700

    single client: small & large call

--- RestAPI/clientA.js
+++ RestAPI/clientA.js
@@ -62,7 +62,12 @@ var scriptName = process.argv.shift();
 var command = process.argv.shift();
 
 if (command == "list") listBook();
-else if (command == "insert")
-  insertBook(process.argv[0], process.argv[1], process.argv[2]);
+else if (command == "insert"){
+  var loop = process.argv[0];
+  if(loop != 1){
+    for(var i = 0; i<loop; i++) insertBook(process.argv[1], process.argv[2], process.argv[3]);
+  }
+  else insertBook(process.argv[1], process.argv[2], process.argv[3]);
+}
 else if (command == "get") getBook(process.argv[0]);
 else if (command == "delete") deleteBook(process.argv[0]);
