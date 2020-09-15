105c9efff143882b21c0bc3e46965edbc58be0b5
Weerayut Thinchamlong <weerayut.thc@gmail.com>
2020-09-16 00:13:44 +0700

    Update iteration function

--- RestAPI/client_a.js
+++ RestAPI/client_a.js
@@ -11,7 +11,7 @@ function getBook(bookID) {
       console.log("Time = ", end - start);
       return;
     })
-    .catch((error) => printError(error))
+    .catch((error) => printError(error));
 }
 
 function listBook() {
@@ -24,25 +24,25 @@ function listBook() {
       console.log("Time = ", end - start);
       return;
     })
-    .catch((error) => printError(error))
+    .catch((error) => printError(error));
 }
 
-function insertBook(bookID, bookTitle, bookAuthor) {
-    const url = `http://localhost:3000/books/insert`;
-    const body = {
-      id: parseInt(bookID),
-      title: bookTitle,
-      author: bookAuthor,
-    }
-  const start = new Date();
-  axios
-    .post(url,body)
+async function insertBook(bookID, bookTitle, bookAuthor) {
+  const url = `http://localhost:3000/books/insert`;
+  const body = {
+    id: parseInt(bookID),
+    title: bookTitle,
+    author: bookAuthor,
+  };
+  // const start = new Date();
+  await axios
+    .post(url, body)
     .then((response) => {
-      const end = new Date();
-      console.log("Time = ", end - start);
-      return;
+      // const end = new Date();
+      // console.log("Time = ", end - start);
+      // return;
     })
-    .catch((error) => printError(error))
+    .catch((error) => printError(error));
 }
 
 function deleteBook(bookID) {
@@ -54,7 +54,15 @@ function deleteBook(bookID) {
       console.log("Time = ", end - start);
       return;
     })
-    .catch((error) => printError(error))
+    .catch((error) => printError(error));
+}
+
+async function insertBookIteration(times, bookID, bookTitle, bookAuthor) {
+  const start_total = new Date();
+  for (let i = 0; i < times; i++)
+    await insertBook(bookID, bookTitle, bookAuthor);
+  const end_total = new Date();
+  console.log("Total time:", end_total - start_total, "ms");
 }
 
 var processName = process.argv.shift();
@@ -62,12 +70,12 @@ var scriptName = process.argv.shift();
 var command = process.argv.shift();
 
 if (command == "list") listBook();
-else if (command == "insert"){
-  var loop = process.argv[0];
-  if(loop != 1){
-    for(var i = 0; i<loop; i++) insertBook(process.argv[1], process.argv[2], process.argv[3]);
-  }
-  else insertBook(process.argv[1], process.argv[2], process.argv[3]);
-}
-else if (command == "get") getBook(process.argv[0]);
+else if (command == "insert") {
+  insertBookIteration(
+    process.argv[0],
+    process.argv[1],
+    process.argv[2],
+    process.argv[3]
+  );
+} else if (command == "get") getBook(process.argv[0]);
 else if (command == "delete") deleteBook(process.argv[0]);
